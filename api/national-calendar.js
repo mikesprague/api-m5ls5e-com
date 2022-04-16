8b3c3e49eaec944d4d0f31fd25496df9f696b0c7
Michael Sprague <mikesprague@gmail.com>
2022-04-16 15:30:05 +0000

    :sparkles: Add national-calendar endpoint

---
+++ api/national-calendar.js
@@ -0,0 +1,78 @@
+const axios = require('axios').default;
+const cheerio = require('cheerio');
+
+module.exports = async (req, res) => {
+  const pageData = await axios
+    .get('http://nationaldaycalendar.com/read/')
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(error);
+    });
+  const page$ = cheerio.load(pageData);
+  const firstLinkSelector =
+    '#ff-main-container > main > article > section > div > div:nth-child(1) > div > div > div:nth-child(1)';
+  const firstItem = page$(firstLinkSelector);
+  const firstLink = page$(firstItem).find('a');
+  const pageUrl = firstLink.attr('href');
+  const allData = [];
+  await axios
+    .get(pageUrl)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      // console.log(response.data);
+      const titleSelectors = ['.ndc-text- h2', '.ndc-text- h3'];
+      titleSelectors.forEach((titleSelector) => {
+        $(titleSelector).each((idx, elem) => {
+          const isBirthday = false;
+          const title = $(elem).text().trim().toUpperCase();
+          const link = $(elem).next('p').find('a').attr('href');
+          const description = $(elem)
+            .next('p')
+            .text()
+            .replace('Read more…', '');
+          if (title.length && description.length && link.length) {
+            allData.push({
+              title,
+              description,
+              link,
+              isBirthday,
+            });
+          }
+        });
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+  await axios
+    .get('https://nationaltoday.com/what-is-today/')
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      // console.log(response.data);
+      const contentSelector = '.day-card .card-content .title-box';
+
+      $(contentSelector).each((idx, elem) => {
+        const title = $(elem).find('a').text().trim().toUpperCase();
+        const description = $(elem).find('p.excerpt').text().trim();
+        const link = $(elem).find('a').attr('href').trim();
+        const isBirthday = link.toLowerCase().includes('birthday');
+        if (title.length && description.length && link.length) {
+          allData.push({
+            title: `${isBirthday ? `BIRTHDAY: ${title}` : title}`,
+            description,
+            link,
+            isBirthday,
+          });
+        }
+      });
+    });
+  const dedupeArrayOfObjects = (objArray, key) => [
+    ...new Map(objArray.map((item) => [item[key], item])).values(),
+  ];
+  const finalData = dedupeArrayOfObjects(allData, 'title');
+  console.log(finalData);
+
+  res.setHeader('Cache-Control', 'max-age=3600, s-maxage=3600');
+  res.status(200).json(finalData);
+};
