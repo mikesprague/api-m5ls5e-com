4f7dd471630dba29775efd9d6f14b3ab42cac864
Michael Sprague <mikesprague@gmail.com>
2022-07-24 09:12:36 -0400

    :recycle: Improve logic

--- api/national-day-calendar.js
+++ api/national-day-calendar.js
@@ -8,50 +8,40 @@ process.env.TZ = 'America/New_York';
 export default async (req, res) => {
   const allData = [];
   const pageData = await axios
-    .get('https://nationaldaycalendar.com/read/')
+    .get('https://nationaldaycalendar.com/')
     .then((response) => response.data)
-    .catch((error) => {
-      console.error(error);
-    });
-  const page$ = cheerio.load(pageData);
-  const firstLinkSelector =
-    '#ff-main-container > main > article > section > div > div:nth-child(1) > div > div > div:nth-child(1)';
-  const firstItem = page$(firstLinkSelector);
-  const firstLink = page$(firstItem).find('a');
-  const pageUrl = firstLink.attr('href');
-  await axios
-    .get(pageUrl)
-    .then((response) => {
-      const $ = cheerio.load(response.data);
-      // console.log(response.data);
-      const titleSelectors = [
-        'main section h1',
-        'main section h2',
-        'main section h3',
-      ];
-      titleSelectors.forEach((titleSelector) => {
-        $(titleSelector).each((idx, elem) => {
-          const title = $(elem).text().toUpperCase().split('|')[0].trim();
-          const link = $(elem).next('p').find('a').attr('href');
-          const description = $(elem)
-            .next('p')
-            .text()
-            .replace('Read more…', '');
-          console.log(title, description, link);
-          if (title && description && link) {
-            allData.push({
-              title,
-              description,
-              link,
-            });
-          }
-        });
-      });
-      res.setHeader('Cache-Control', 'max-age=3600, s-maxage=3600');
-      res.status(200).json(allData);
-    })
     .catch((error) => {
       console.error(error);
       res.status(500).json(error);
     });
+  const $ = cheerio.load(pageData);
+  const groupSelector = 'div.sep_month_events';
+  const groupData = $(groupSelector);
+  const days = $(groupData).find(
+    '.eventon_list_event.evo_eventtop.scheduled.event',
+  );
+  for await (const day of $(days)) {
+    const title = $(day).find('span.evcal_event_title').text().trim();
+    const link = $(day).find('a').first().attr('href').trim();
+    // console.log(title, link);
+    const descriptionData = await axios(link)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json(error);
+      });
+    const selector = '#ff-main-container > main > article > section';
+    const $desc = cheerio.load(descriptionData);
+    const description = $desc(selector).find('p').first().text().trim();
+    // console.log(description);
+    if (title && link && description) {
+      allData.push({
+        title,
+        link,
+        description,
+      });
+    }
+  }
+  res.setHeader('Cache-Control', 'max-age=3600, s-maxage=3600');
+  res.status(200).json(allData);
 };
